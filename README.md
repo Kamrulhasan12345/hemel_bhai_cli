0a8b090762d21888ad930110c597b6d6692c740a
Mohammad Kamrul Hasan <kamrulhasan59246@gmail.com>
2023-10-08 20:56:13 +0600

    [fix]: fix some minor issues

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# hemel_vai_dl
+# hemel_bhai_cli
 
 A Command-line Interface (CLI) for downloading A Command-line Interface (CLI) for downloading PDF Lecture files from [Learn with Hemel Bhai](https://www.youtube.com/@Hemel_Bhai) YouTube Channel.
--- index.ts
+++ index.ts
@@ -4,17 +4,25 @@ import colors from "ansi-colors";
 import enquirer from "enquirer";
 import figures from "figures";
 import fs from "node:fs";
-import Miniget from "miniget";
 import { Innertube } from "youtubei.js/web";
 import ProgressBar from "progress";
+import { promisify } from "node:util";
 import sanitize from "sanitize-filename";
+import stream from "node:stream";
+import axios from "axios";
+import ora from "ora";
 
 const CHANNEL_ID = "UCUZ1Jxh8roXc1JJh4PXBF7w";
 const LOCALE = "en-US-u-co-bn";
 
+const pipeline = promisify(stream.pipeline);
+
 const main = async () => {
+  const spinner = ora("Intitializing...").start();
   const yt = await Innertube.create();
   const channel = await yt.getChannel(CHANNEL_ID);
+  spinner.color = "green";
+  spinner.text = "Fetching playlists...";
   const playlists = (await channel.getPlaylists()).playlists
     .filter(
       (v, i, a) =>
@@ -31,6 +39,8 @@ const main = async () => {
         (/\d+/g.exec(v.video_count.text as string)[0] as string);
       return v;
     });
+
+  spinner.stop();
 
   const choices = playlists.map((v) => ({
     name: v.title.text as string,
@@ -75,36 +85,55 @@ const main = async () => {
       "     :bar :percent :currVideos/:totVideos lectures eta :etas",
       {
         total: videos.length,
-        clear: false,
+        clear: true,
         width: 100,
         complete: colors.green("━"),
         incomplete: colors.white("━"),
       },
     );
     const folderName = sanitize(selectedPlaylists[i].title.text as string);
-    try {
-      await fs.promises.mkdir(folderName);
-    } finally {
-      for (let j = 0; j < videos.length; j++) {
-        const videoDescParts = (
-          await yt.getBasicInfo((videos[j] as any).id as string)
-        ).basic_info.short_description?.split("\n");
-        if (videoDescParts) {
-          let pdfUrl: string = "";
-          const pdfIndex = videoDescParts.findIndex((part) =>
-            part.includes("PDF"),
+    if (!fs.existsSync(folderName)) await fs.promises.mkdir(folderName);
+    for (let j = 0; j < videos.length; j++) {
+      const videoDescParts = (
+        await yt.getBasicInfo((videos[j] as any).id as string)
+      ).basic_info.short_description?.split("\n");
+      if (videoDescParts) {
+        let pdfUrl: string = "";
+        const pdfIndex = videoDescParts.findIndex((part) =>
+          part.includes("PDF"),
+        );
+        if (pdfIndex !== -1 && pdfIndex < videoDescParts.length - 1) {
+          const urlParts = videoDescParts[pdfIndex + 1].split("/");
+          const id = urlParts[urlParts.length - 2];
+          pdfUrl = `https://drive.google.com/uc?id=${id}&export=download`;
+          const fileName = `./${folderName}/${j + 1}.pdf`;
+          const writer = fs.createWriteStream(fileName);
+          const reader = await axios.get(pdfUrl, { responseType: "stream" });
+          writer.on("finish", () => {
+            writer.close();
+            bar.tick(0, { currVideos: j + 1, totVideos: videos.length });
+          });
+          writer.on("error", (err) => {
+            fs.unlink(fileName, (err) => console.error(err));
+            console.error(err);
+          });
+          reader.data.on("data", (chunk: any) =>
+            bar.tick(chunk.length / Number(reader.headers["content-length"]), {
+              currVideos: j,
+              totVideos: videos.length,
+            }),
           );
-          if (pdfIndex !== -1 && pdfIndex < videoDescParts.length - 1) {
-            const urlParts = videoDescParts[pdfIndex + 1].split("/");
-            const id = urlParts[urlParts.length - 2];
-            pdfUrl = `https://drive.google.com/uc?id=${id}&export=download`;
-            const writer = fs.createWriteStream(`./${folderName}/${j + 1}.pdf`);
-            await Miniget(pdfUrl).pipe(writer);
-          }
-          bar.tick({ currVideos: j + 1, totVideos: videos.length });
-        }
+          reader.data.on("error", (err: any) => {
+            fs.unlink(fileName, (err) => console.error(err));
+            console.error(err);
+          });
+          await pipeline(reader.data, writer).then(
+            () => reader.data?.destroy(),
+          );
+        } else bar.tick({ currVideos: j, totVideos: videos.length });
       }
     }
+    console.log(colors.green("\nSuccessfully downloaded %s"), selectedPlaylists[i].title?.text);
   }
 };
 
